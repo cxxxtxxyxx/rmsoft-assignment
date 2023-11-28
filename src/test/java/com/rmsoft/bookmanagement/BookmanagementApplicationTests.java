9a1e32233a9047e883fece14122213d51dabcd2f
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-11-28 18:45:02 +0900

    chore: project setting

---
+++ src/test/java/com/rmsoft/bookmanagement/BookmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmsoft.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
